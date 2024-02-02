165bf9addded97605b079b1243c945d1cc82d160
lvitorinogoncalves <lvitorinogoncalves@gmail.com>
2024-02-02 08:49:31 -0300

    Projeto scala para teste de códigos

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,10 @@
+
+9
+	build.sbt,7/4/74b95601c3a6d9d565cd312fac64094ec0f9d847
+I
+src/main/scala/main.scala,f/5/f5e4d4e231418b789b9ee656d552344bce983527
+M
+src/main/scala/RunCodes.scala,6/f/6f49df233ac6f6db6a03217a8f9dcdde242fa070
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/main/scala/RunCodes.scala
@@ -0,0 +1,36 @@
+object RunCodes {
+  def runCodes(): Unit = {
+    val animalsList: List[String] = List("cat", "dog", "fish", "bird", "mouse")
+    val numbres: List[Int] = List(1,2,3,4,5,6,7,8,9)
+
+    val animalTarget = "dog"
+    val animalFound = animalsList.find(_ == animalTarget)
+
+    println(animalFound)
+
+//    animalFound match
+//      case Some(animalFoundOk) =>
+//        println(s"Animal '$animalFoundOk' was found")
+//      case None =>
+//        println(s"Animal '$animalTarget' not found")
+
+    val resultNumbersList = multiplyNumbers(numbres)
+    println(resultNumbersList)
+
+    val list1: List[String] = List("cat", "dog", "bird", "mouse", "lion", "tiger", "elephant", "alligator")
+    val list2: List[String] = List("cat", "dog", "bird", "mouse")
+
+    val resultVerifyPetAnimals = findPetAnimals(list1, list2)
+    println(resultVerifyPetAnimals)
+  }
+
+  def multiplyNumbers(listNumbers: List[Int]): List[Int] = {
+    listNumbers.map(_ * 2)
+  }
+
+  def findPetAnimals(list1: List[String], list2: List[String]): List[String] = {
+    val (largerList, smallerList) = if (list1.length >= list2.length) (list1, list2) else (list2, list1)
+    val listPets = smallerList.filter(largerList.contains)
+    listPets.map(item => item + " pode ser um pet")
+  }
+}
