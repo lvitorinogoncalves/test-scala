0ae64268f724e84384fdb7ec6321d6ad9b74e6b6
lvitorinogoncalves <lvitorinogoncalves@gmail.com>
2024-02-02 11:49:02 -0300

    Alterado versão do scala e refatorado o pacote das classes de teste

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,8 +2,13 @@ import testsCodes.{RunCodes, TransformData}
 
 object main {
   def main(agrs: Array[String]): Unit = {
+    testsCodes()
+  }
+
+  def testsCodes(): Unit = {
     println("API-Scala")
     RunCodes.runCodes()
+
     val transformData = new TransformData
     transformData.readAndTransformData()
   }
